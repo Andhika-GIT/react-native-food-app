a2d7e78f8f753826b13b3d36d3eaacb1edd98d12
Andhika Prasetya <andhikapr081@gmail.com>
2023-05-20 14:35:52 +0700

    setups and import assets

---
+++ food-frontend-rn/src/HomeScreen.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+// pages
+import { SplashScreen } from './pages';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <SplashScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
