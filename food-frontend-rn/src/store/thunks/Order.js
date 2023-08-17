55fa216c5fa64ce0e11846610e494bcac7a5906f
Andhika-GIT <andhikapr081@gmail.com>
2023-08-17 20:05:34 +0700

    frontend update order action creator create multiple request using axios

--- food-frontend-rn/src/store/thunks/Order.js
+++ food-frontend-rn/src/store/thunks/Order.js
@@ -26,21 +26,80 @@ export const getOrders = createAsyncThunk('order/getOrders', async (data, { disp
     .catch((err) => console.log(err));
 });
 
+// 37478329105
+
+//
+
 export const getInProgress = createAsyncThunk('order/getInProgress', async (data, { dispatch }) => {
+  const resToken = await getData('token');
+  console.log(resToken.value);
+  axios
+    .all([
+      axios.get(`${API_URL}/transaction`, {
+        headers: {
+          Authorization: resToken.value,
+        },
+        params: {
+          status: 'PENDING',
+        },
+      }),
+      axios.get(`${API_URL}/transaction`, {
+        headers: {
+          Authorization: resToken.value,
+        },
+        params: {
+          status: 'SUCCESS',
+        },
+      }),
+      axios.get(`${API_URL}/transaction`, {
+        headers: {
+          Authorization: resToken.value,
+        },
+        params: {
+          status: 'ON_DELIVERY',
+        },
+      }),
+    ])
+    .then(
+      axios.spread((resPending, resSuccess, resOnDelivery) => {
+        const pending = resPending.data.data.data;
+        const success = resSuccess.data.data.data;
+        const onDelivery = resOnDelivery.data.data.data;
+        dispatch(setInProgress([...pending, ...success, ...onDelivery]));
+      })
+    )
+    .catch((err) => console.log(err));
+});
+export const getPastOrders = createAsyncThunk('order/getPastOrders', async (data, { dispatch }) => {
   const resToken = await getData('token');
   console.log(resToken.value);
 
   axios
-    .get(`${API_URL}/transaction`, {
-      headers: {
-        Authorization: resToken.value,
-      },
-      params: {
-        status: 'PENDING',
-      },
-    })
-    .then((res) => {
-      dispatch(setInProgress(res.data.data.data));
-    })
+    .all([
+      axios.get(`${API_URL}/transaction`, {
+        headers: {
+          Authorization: resToken.value,
+        },
+        params: {
+          status: 'CANCELLED',
+        },
+      }),
+      axios.get(`${API_URL}/transaction`, {
+        headers: {
+          Authorization: resToken.value,
+        },
+        params: {
+          status: 'DELIVERED',
+        },
+      }),
+    ])
+    .then(
+      axios.spread((resCancelled, resDelivered) => {
+        const cancelled = resCancelled.data.data.data;
+        const delivered = resDelivered.data.data.data;
+
+        dispatch(setPastOrders([...cancelled, ...delivered]));
+      })
+    )
     .catch((err) => console.log(err));
 });
